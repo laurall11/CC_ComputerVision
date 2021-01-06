8551835f3b9c689e9155cda45ee8ffc3b6f3fe28
Laura Legat <laura.legat11@gmail.com>
2021-01-06 20:31:26 +0100

    initial commit

---
+++ CVConsole/CVConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CVConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
