cb65fbe4e5695572f8ad7def0fd281d1d080af90
Flo <pan-pan@live.de>
2021-01-08 23:59:35 +0100

    Backend

---
+++ CC.ComputerVision/CC.ComputerVision.Console/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace CC.ComputerVision.BackendConsole
+{
+    class Program
+    {
+        // Add your Computer Vision subscription key and endpoint
+        static string subscriptionKey = Environment.GetEnvironmentVariable("78556231cd4c409a928718285db5316c");
+        static string endpoint = Environment.GetEnvironmentVariable("https://ccsp.cognitiveservices.azure.com/");
+
+        // URL image used for analyzing an image (image of puppy)
+        private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting analyzing service");
+
+            // Create a client
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+            // Analyze an image to get features and other properties.
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+
+            // Extract text (OCR) from a URL image using the Read API
+            // ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
+            // Extract text (OCR) from a local image using the Read API
+            // ReadFileLocal(client, READ_TEXT_LOCAL_IMAGE).Wait();
+
+
+        }
+        //Authenticate Client
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+                new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+                    { Endpoint = endpoint };
+            return client;
+        }
+
+        /* 
+         * ANALYZE IMAGE - URL IMAGE
+         * Analyze URL image. Extracts captions, categories, tags, objects, faces, racy/adult content,
+         * brands, celebrities, landmarks, color scheme, and image types.
+         */
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
+            Console.WriteLine();
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
+
+            Console.WriteLine(results);
+        }
+    }
+}
