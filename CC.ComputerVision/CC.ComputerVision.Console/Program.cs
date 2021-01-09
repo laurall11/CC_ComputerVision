54bacc8bbbdff392415de92f66ee30cbebfce26e
Flo <pan-pan@live.de>
2021-01-10 00:30:49 +0100

    added local method

--- CC.ComputerVision/CC.ComputerVision.Console/Program.cs
+++ CC.ComputerVision/CC.ComputerVision.Console/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Threading;
@@ -16,9 +18,11 @@ namespace CC.AzureVision.BackendConsole
         // Add your Computer Vision subscription key and endpoint
         public static string subscriptionKey = "78556231cd4c409a928718285db5316c";
         public static string endpoint = "https://ccsp.cognitiveservices.azure.com/";
+        public static ImageAnalysis result;
 
         // URL image used for analyzing an image (image of puppy)
-        private const string ANALYZE_URL_IMAGE = "C:\\Users\\Flo\\AppData\\Local\\Temp\\tmpB172.png";
+        private const string ANALYZE_URL_IMAGE = "C:\\Users\\Flo\\Downloads\\test.jpg";
+        private const string ANALYZE_LOCAL_IMAGE = "C:\\Users\\Flo\\AppData\\Local\\Temp\\tmpB172.tmp";
 
         static void Main(string[] args)
         {
@@ -28,21 +32,33 @@ namespace CC.AzureVision.BackendConsole
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
 
             // Analyze an image to get features and other properties.
-            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
 
+            // AnalyzeImageLocal(client, ANALYZE_LOCAL_IMAGE).Wait();
+
+            // AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
             // Extract text (OCR) from a URL image using the Read API
             // ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
             // Extract text (OCR) from a local image using the Read API
             // ReadFileLocal(client, READ_TEXT_LOCAL_IMAGE).Wait();
 
-
+            AnalyzeLocalImageFromApi(ANALYZE_LOCAL_IMAGE);
         }
+
+        public static string AnalyzeLocalImageFromApi(string filePath)
+        {
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+            AnalyzeImageLocal(client, filePath).Wait();
+
+            return result.ToString();
+        }
+
         //Authenticate Client
         public static ComputerVisionClient Authenticate(string endpoint, string key)
         {
             ComputerVisionClient client =
                 new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-                    { Endpoint = endpoint };
+                { Endpoint = endpoint };
             return client;
         }
 
@@ -74,6 +90,50 @@ namespace CC.AzureVision.BackendConsole
             ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
 
             Console.WriteLine(results);
+        }
+
+        public static async Task AnalyzeImageLocal(ComputerVisionClient client, string imageLocal)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - LOCAL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageLocal)}...");
+            Console.WriteLine();
+
+            using (Stream analyzeImageStream = File.OpenRead(imageLocal))
+            {
+                // Analyze the URL image 
+                ImageAnalysis results = await client.AnalyzeImageInStreamAsync(analyzeImageStream, features);
+
+                result = results;
+                
+                Console.WriteLine(results);
+            }
+
+        }
+
+        static async Task SynthesizeAudioAsync(string toRead)
+        {
+            string subscription = "9452bdccf9f54506afb6c7affd3067bd";
+            string region = "westeurope";
+            var config = SpeechConfig.FromSubscription(subscription, region);
+            using var audioConfig = AudioConfig.FromWavFileOutput(System.AppDomain.CurrentDomain.BaseDirectory + @"hello.wav"); //Path to File
+            using var synthesizer = new SpeechSynthesizer(config, audioConfig); //using for saving a file
+            //using var synthesizer = new SpeechSynthesizer(config); //using without saving a file
+
+            await synthesizer.SpeakTextAsync(toRead);
         }
     }
 }
