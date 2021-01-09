1086a242d3dc844906f8b2c5674125f3e2256c81
Laura Legat <laura.legat11@gmail.com>
2021-01-09 23:29:34 +0100

    headers removed

--- CV_FE/cv-frontend/src/app/upload.service.ts
+++ CV_FE/cv-frontend/src/app/upload.service.ts
@@ -2,13 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import { HttpHeaders } from '@angular/common/http';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'image/*'
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +13,6 @@ export class UploadService {
     const endpoint = 'https://localhost:44336/api/analyzeImage';
     const formData: FormData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
-    return this.http.post<File>(endpoint, formData, httpOptions);
+    return this.http.post<File>(endpoint, formData);
   }
 }
