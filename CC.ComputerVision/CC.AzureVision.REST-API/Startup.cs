43f2021a57a1cab702fa56ce7c60e8b83d12aaf8
Laura Legat <laura.legat11@gmail.com>
2021-01-09 22:32:57 +0100

    CORS added

--- CC.ComputerVision/CC.AzureVision.REST-API/Startup.cs
+++ CC.ComputerVision/CC.AzureVision.REST-API/Startup.cs
@@ -15,6 +15,8 @@ namespace CC.AzureVision.REST_API
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +27,17 @@ namespace CC.AzureVision.REST_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:4200")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod(); ;
+                                  });
+            });
+
             services.AddControllers();
         }
 
@@ -39,6 +52,8 @@ namespace CC.AzureVision.REST_API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
