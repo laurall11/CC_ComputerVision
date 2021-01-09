2838a35480b1fa3779bad3189f2418f0c47dbec9
Flo <pan-pan@live.de>
2021-01-09 23:44:47 +0100

    file upload fix

--- CC.ComputerVision/CC.AzureVision.REST-API/Controllers/AzureVisionController.cs
+++ CC.ComputerVision/CC.AzureVision.REST-API/Controllers/AzureVisionController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,8 +26,33 @@ namespace CC.AzureVision.REST_API.Controllers
         }
 
         [HttpPost("/api/analyzeImage")]
-        public string AnalzyeImage([FromForm] object image)
+        public async Task<IActionResult> OnPostUploadAsync([FromForm] List<IFormFile> file)
         {
+            long size = file.Sum(f => f.Length);
+
+            foreach (var formFile in file)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = file.Count, size });
+        }
+
+
+        public string AnalzyeImage()
+        {
+            IFormCollection test = Request.Form;
             return "hallo";
         }
     }
