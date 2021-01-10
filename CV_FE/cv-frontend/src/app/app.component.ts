fafa9dbf3f5c97a94ff6dcf080d181135c1f7393
Laura Legat <laura.legat11@gmail.com>
2021-01-10 23:57:11 +0100

    picture display added

--- CV_FE/cv-frontend/src/app/app.component.ts
+++ CV_FE/cv-frontend/src/app/app.component.ts
@@ -12,23 +12,31 @@ export class AppComponent {
   title = 'cv-frontend';
   fileToUpload: any = null;
   resultFiles : any[] = [];
-  audioCtx = new window.AudioContext();
-  context = new AudioContext();
 
 onFileUpload(files: Event){
   this.fileToUpload = files.target;
   this.fileToUpload = this.fileToUpload.files.item(0);
+
+  //immediately display image on page after selecting
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    var img = document.getElementById('img-prev');
+    img?.setAttribute('src', e.target?.result as string);
+    img?.setAttribute('style', 'max-height: 500px; max-width: 500px;');
+  }
+
+  reader.readAsDataURL(this.fileToUpload);
+
+  console.log(this.fileToUpload);
   this.uploadService
     .upload(this.fileToUpload)
     .subscribe(result => {
 
-      result.arrayBuffer().then((resultat: ArrayBuffer) =>{
-
-        this.audioCtx.decodeAudioData(resultat).then(function (decodedData) {
+      var audioCtx = new window.AudioContext();
+        var hm = audioCtx.decodeAudioData(result).then(function (decodedData) {
           console.log(decodedData);
         });
-
-      });
 
       this.resultFiles.push(result)});
 }
