bacc690f9c45525e14be6cf69053fcc956927210
Flo <pan-pan@live.de>
2021-01-12 17:23:33 +0100

    testing

--- CV_FE/cv-frontend/src/app/app.component.ts
+++ CV_FE/cv-frontend/src/app/app.component.ts
@@ -8,29 +8,63 @@ import { UploadService} from './upload.service';
 })
 export class AppComponent {
   constructor(private uploadService: UploadService) { }
-
+  
   title = 'cv-frontend';
   fileToUpload: any = null;
   resultFiles : any[] = [];
   audioCtx = new window.AudioContext();
   context = new AudioContext();
-
-onFileUpload(files: Event){
-  this.fileToUpload = files.target;
-  this.fileToUpload = this.fileToUpload.files.item(0);
-  this.uploadService
+  fr = new FileReader();
+  bf = new ArrayBuffer(1210892);
+  
+  
+  
+  onFileUpload(files: Event){
+    this.fileToUpload = files.target;
+    this.fileToUpload = this.fileToUpload.files.item(0);
+    this.uploadService
     .upload(this.fileToUpload)
     .subscribe(result => {
-
-      result.arrayBuffer().then((resultat: ArrayBuffer) =>{
-
-        this.audioCtx.decodeAudioData(resultat).then(function (decodedData) {
-          console.log(decodedData);
-        });
-
+      
+      
+      
+      
+      this.audioCtx.decodeAudioData(result.data).then(function (decodedData) {
+        console.log(decodedData);
       });
-
+      
+      
+      
       this.resultFiles.push(result)});
-}
+    }
+    
+    test(files) {
+      
+      let buffer = new ArrayBuffer(1210892);
+      let audioCtx = new window.AudioContext();
+      let context = new AudioContext();
 
-}
+      
+      this.fileToUpload = files.target;
+      this.fileToUpload = this.fileToUpload.files.item(0);
+
+    
+      let reader = new FileReader();
+      
+    
+      reader.readAsArrayBuffer(this.fileToUpload);
+
+      reader.onload = function() {
+        console.log(reader.result);
+        buffer = reader.result as ArrayBuffer;
+        audioCtx.decodeAudioData(buffer).then(function (decodedData) {
+          console.log(decodedData)
+      });
+      
+      reader.onerror = function() {
+        console.log(reader.error);
+      };
+      
+    }
+  }
+  }
--- CV_FE/cv-frontend/src/app/upload.service.ts
+++ CV_FE/cv-frontend/src/app/upload.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+
+const httpOptions = {
+  responseType: "blob" as const
+};
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +14,9 @@ export class UploadService {
   constructor(private http: HttpClient) { }
 
   upload(fileToUpload: File): Observable<any> {
-    const endpoint = 'https://localhost:44336/api/analyzeImage';
+    const endpoint = 'http://localhost:59089/api/analyzeImage';
     const formData: FormData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
-    return this.http.post(endpoint, formData, {responseType: "arraybuffer"});
+    return this.http.post(endpoint, formData, httpOptions);
   }
 }
