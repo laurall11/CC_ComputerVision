2bc57035c3a8ceffcee40ec487b9adcd4afb06cd
Laura Legat <laura.legat11@gmail.com>
2021-01-13 02:43:41 +0100

    multiple files upload fix

--- CV_FE/cv-frontend/src/app/app.component.ts
+++ CV_FE/cv-frontend/src/app/app.component.ts
@@ -8,19 +8,35 @@ import { UploadService} from './upload.service';
 })
 export class AppComponent {
   constructor(private uploadService: UploadService) { }
-  
   title = 'cv-frontend';
+
   fileToUpload: any = null;
-  resultFiles : any[] = [];
 
 onFileUpload(files: Event){
 
+  //remove all previous elements and stop previous audio
+  var description : HTMLParagraphElement = document.getElementById('description') as HTMLParagraphElement;
+  description.innerHTML = "";
+
+  var image : HTMLImageElement = document.getElementById('img-prev') as HTMLImageElement;
+  image.src = "";
+
+  //get div with audio player in it and remove old audio player
+  var audiodiv : HTMLDivElement = document.getElementById('audioDiv') as HTMLDivElement;
+  if (audiodiv.hasChildNodes()) {
+    var lastAudio: HTMLAudioElement = audiodiv.lastChild as HTMLAudioElement;
+    lastAudio.pause();
+    lastAudio.remove();
+  }
+
+  //show loading text
   document.getElementById('loadingNotif')?.setAttribute('style', 'display: initial');
 
+  //get file out of filearray
   this.fileToUpload = files.target;
   this.fileToUpload = this.fileToUpload.files.item(0);
 
-  //shoe image immediately
+  //show uploaded image immediately
   var reader = new FileReader();
 
   reader.onload = function (e) {
@@ -31,12 +47,16 @@ onFileUpload(files: Event){
 
   reader.readAsDataURL(this.fileToUpload);
 
+  //send image to backend
   this.uploadService
     .upload(this.fileToUpload)
     .subscribe(result => {
+
+      //get created audio
       this.getAudio();
     });
     }
+
 
   getAudio() {
 
@@ -59,8 +79,7 @@ onFileUpload(files: Event){
         let reader = new FileReader();
         let audioCtxx = new window.AudioContext();
         var source = audioCtxx.createBufferSource();
-        var audio = document.getElementById('audioPlayer');
-        var audioplayer = document.getElementById('audioSrc');
+
 
         reader.readAsArrayBuffer(getResult);
         reader.onload = function () {
@@ -69,16 +88,23 @@ onFileUpload(files: Event){
           audioCtxx.decodeAudioData(buffer, function (buff) {
             source.buffer = buff;
             source.connect(audioCtxx.destination);
-            console.log(audioplayer);
+
+            //create new audio element
+            var audiodiv: HTMLDivElement = document.getElementById('audioDiv') as HTMLDivElement;
+
+            var audioPlayer = document.createElement('audio');
+            audioPlayer.controls = true;
+            audioPlayer.autoplay = true;
 
             //create src element
             const url = window.URL.createObjectURL(getResult);
 
             var sourcee = document.createElement('source');
+
             sourcee.src = url;
 
-            audio?.appendChild(sourcee);
-            audio?.setAttribute('style', 'display: initial;');
+            audioPlayer?.appendChild(sourcee);
+            audiodiv?.appendChild(audioPlayer);
             document.getElementById('loadingNotif')?.setAttribute('style', 'display: none');
           });
         }
